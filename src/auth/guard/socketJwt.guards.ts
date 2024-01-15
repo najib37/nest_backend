ff1ee5c95a01c18176581ee6020a19f42f6011dc
najib37 <najibramadan37@gmail.com>
2024-01-15 20:16:55 +0000

    added the sockerGuard

---
+++ src/auth/guard/socketJwt.guards.ts
@@ -0,0 +1,39 @@
+import { ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { AuthGuard } from "@nestjs/passport";
+
+@Injectable()
+export class SocketJwtGuard extends AuthGuard('jwt') {
+  constructor(private jwtService: JwtService,
+    private config: ConfigService
+  ) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const ctx = context.switchToWs();
+    const client = ctx.getClient();
+    const cookie: string = client.handshake.headers.cookie;
+
+    const accessToken = cookie?.replace('jwt=', '');
+
+    if (!accessToken) {
+      client.emit("Error", "Unauthorized") // emit an an errorMessage
+      return false;
+    }
+    try {
+      const payload = await this.jwtService.verify(accessToken, {
+        secret: this.config.get<string>('SECRET')
+      })
+      client.user = payload;
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+    }
+    catch (err) {
+      client.emit("Error", "Unauthorized")
+      return false;
+    }
+    return true;
+  }
+}
