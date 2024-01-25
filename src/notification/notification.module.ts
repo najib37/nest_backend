1af98a02fd9b16c5a1f6e32c20e5bf585c33b6dd
najib37 <najibramadan37@gmail.com>
2024-01-25 20:08:14 +0000

    changed the notification module

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -4,6 +4,7 @@ import { NotificationGateway } from './notification.gateway';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserModule } from 'src/user/user.module';
 
 @Global()
 @Module({
