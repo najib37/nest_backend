fe98d8bd3a14689cc41f16146f582ea82643fa67
najib37 <najibramadan37@gmail.com>
2024-02-18 14:40:59 +0000

    added the block controller to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,34 @@ import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule} from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config'
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from './user/user.module';
 import { ImagesRepoModule } from './images-repo/images-repo.module';
 import { NotificationModule } from './notification/notification.module';
 import { FriendsModule } from './friends/friends.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ProfileModule } from './profile/profile.module';
+import { BlockModule } from './block/block.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { GameModule } from './game/game.module';
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal : true,
-  }),  AuthModule, UserModule, ImagesRepoModule, FriendsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    PrismaModule,
+    NotificationModule,
+    UserModule,
+    ImagesRepoModule,
+    BlockModule,
+    FriendsModule,
+    ProfileModule,
+    GameModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, Logger ],
+  providers: [AppService, Logger],
   exports: [Logger]
 })
-export class AppModule {}
+export class AppModule { }
